656c2e6dd18c97e5dfd6e7437398b4fbce5fb262
Thyago <thyago@usto.re>
2018-11-20 09:09:31 -0300

    [Front] A mizerra do angular 2 é sensivel de mais, quebrou mas eu recreir o modulo do front

---
+++ angular-and-node/ang-node/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { PostsComponent } from './posts/posts.component';
+
+const Routes = [
+
+  {
+    path: '' , 
+    redirecTo: 'posts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'posts' , 
+    component: PostsComponent
+  }
+
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostsComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(Routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular-and-node/ang-node/src/app/posts.service.spec.ts
@@ -0,0 +1,14 @@
+import { TestBed, async, inject } from '@angular/core/testing';
+import { PostsService } from './posts.service';
+
+describe('PostsService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [PostsService]
+    });
+  });
+
+  it('should ...', inject([PostsService], (service: PostsService) => {
+    expect(service).toBeTruthy();
+  }));
+});
