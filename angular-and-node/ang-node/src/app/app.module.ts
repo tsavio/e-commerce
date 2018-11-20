ab8b9d983f396fd590f175126296c7680e80f1e8
Thyago <thyago@usto.re>
2018-11-20 10:11:03 -0300

    [Front] Correção na abertura de dashboard

--- angular-and-node/ang-node/src/app/app.module.ts
+++ angular-and-node/ang-node/src/app/app.module.ts
@@ -4,32 +4,16 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { PostsComponent } from './posts/posts.component';
-
-const Routes = [
-
-  {
-    path: '' , 
-    redirecTo: 'posts',
-    pathMatch: 'full'
-  },
-  {
-    path: 'posts' , 
-    component: PostsComponent
-  }
-
-];
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent,
     DashboardComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(Routes)
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
